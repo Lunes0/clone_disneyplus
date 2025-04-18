cf9965101e811016a746dcc4dc2a2451c3173966
Lunes0 <lunes6660@gmail.com>
2025-04-17 23:01:10 -0300

    Adicionado a seção FAQ

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(buttonActive) {
@@ -9,22 +10,32 @@ document.addEventListener("DOMContentLoaded", function () {
             tab.classList.add('shows__list--is-active');
             removeActiveClass();
             buttonActive.target.classList.add('shows__tabs__button--is-active');
-        })
-    }
+        });
+    };
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', toggleAnswer);
+    };
 });
+
+function toggleAnswer(event) {
+    const classFaq = 'faq__questions__item--is-open';
+    const eventTarget = event.target.parentNode;
+
+    eventTarget.classList.toggle(classFaq);
+};
 
 function removeActiveClass() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active')
-    }
-}
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    };
+};
 
 function hideTabs() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is-active')
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    };
 
-    }
-
-}
+};
